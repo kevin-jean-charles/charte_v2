c58bc5740c38f24808de52b52fd8133b84793ad0
Kevin <kevin.jean-charles@outlook.fr>
2021-02-02 01:22:52 +0100

    commit charte v2

---
+++ js/index.js
@@ -0,0 +1,11 @@
+window.load = function (){
+    
+    //tableau des apprentants
+    var listeApprenant =  ['prenom1','prenom']; 
+    
+    //donner la valeur btn-random a
+    var btRandom = document.getElementById('btn-random'); 
+    
+    //affiche la valeur de btnRandom 
+    console.log(btRandom);
+}
